74ae81c8f0ce9a5de9e6873b58eae63cbf6af8b2
Cathy Qin <qin.xuj@northeastern.edu>
2024-04-03 18:45:46 -0700

    gui design done

---
+++ src/reaction/Mass.java
@@ -0,0 +1,34 @@
+package reaction;
+
+import graphics.G;
+import music.I;
+
+import java.awt.*;
+
+/**
+ * @create 2022/10/11
+ */
+public abstract class Mass extends Reaction.List implements I.Show {
+    public Layer layer;
+    public int hashcode = G.rnd(100000000);
+    public boolean equals(Object obj){
+        return this == obj;
+    }
+    public int hashCode(){
+        return hashcode;
+    }
+    public Mass(String layerName){
+        this.layer = Layer.byName.get(layerName);
+        if(layer != null){
+            layer.add(this);
+        }else{
+            System.out.print("Bad layer name" + layerName);
+        }
+    }
+    public void deleteMass(){
+        clearAll();
+        layer.remove(this);
+    }
+    public void show(Graphics g){}
+}
+
---
+++ src/sandbox/Main.java
@@ -0,0 +1,15 @@
+package sandbox;
+
+import graphics.Window;
+
+/**
+ * @create 2022/9/13
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("hello music");
+        Window.PANEL = new ShapeTrainer();//只需要看window调用的是paint还是squares
+        Window.launch() ;
+
+    }
+}
